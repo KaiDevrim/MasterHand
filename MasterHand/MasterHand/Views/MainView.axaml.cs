e2e3e7b95c2741d163d16c67dda55976e3b91578
Kai Devrim <kai@devrim.tech>
2023-08-28 21:11:47 -0700

    Remove unused Avalonia application files
    This commit deletes a variety of unused files associated with the Avalonia Application named MasterHand. The removed files include: Android resources, iOS resources, and web application resources such as CSS files, HTML files, animations, and images. These files are being removed as they are no longer necessary for the application's current functionality. This cleanup improves the project structure and reduces the size of the project.

--- MasterHand/MasterHand/Views/MainView.axaml.cs
+++ MasterHand/MasterHand/Views/MainView.axaml.cs
@@ -2,10 +2,18 @@ using Avalonia.Controls;
 
 namespace MasterHand.Views;
 
+using System;
+using Avalonia.Interactivity;
+
 public partial class MainView : UserControl
 {
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("Hi I clicked a button!");
     }
 }
